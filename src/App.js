241dcd2604571a589485b8ca23aa11541b2b1791
Кирилл Гребень <atrakir@gmail.com>
2020-12-06 16:24:49 +0200

    filter added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import { Component } from 'react';
 
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   handleAddContact = contact => {
@@ -27,8 +34,20 @@ class App extends Component {
     }));
   };
 
+  handleFilterChange = filter => {
+    this.setState({ filter });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
+
   render() {
-    const { contacts } = this.state;
+    const { filter } = this.state;
+    const filteredContacts = this.getFilteredContacts();
     return (
       <div>
         <h1>Phonebook</h1>
@@ -38,7 +57,12 @@ class App extends Component {
         />
 
         <h2>Contacts</h2>
-        <ContactList contacts={contacts} onRemove={this.handleRemoveContact} />
+        <h3>Find contacts by name</h3>
+        <Filter filter={filter} onChange={this.handleFilterChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onRemove={this.handleRemoveContact}
+        />
       </div>
     );
   }
